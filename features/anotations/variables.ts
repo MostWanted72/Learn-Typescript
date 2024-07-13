3b6df3927d611efde59b6f11ecd74b6e65e7b8c8
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-07-13 15:21:19 +0530

    when to add type inferance

--- features/anotations/variables.ts
+++ features/anotations/variables.ts
@@ -13,3 +13,26 @@ let point: { x: number; y: number } = {
   x: 3,
   y: 5,
 };
+
+// funtion
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// when do you use anotations
+// 1) when the functions returns any type
+const json = '{ "x": 10, "y": 20 }';
+const coordinates: { x: 10; y: 20 } = JSON.parse(json);
+
+console.log(json);
+
+// 2) when we declare the variable in one line
+// ande initialize in another
+let words = ['green', 'red', 'blue'];
+let foundWords: boolean;
+
+for (let i = 0; i < words.length; i += 1) {
+  if (words[i] === 'blue') {
+    foundWords = true;
+  }
+}
