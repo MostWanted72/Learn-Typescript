42548d17573d281ba6eda5fb69cba4966aa3c114
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-07-13 17:02:18 +0530

    void and never

--- features/anotations/functions.ts
+++ features/anotations/functions.ts
@@ -5,3 +5,15 @@ const add = (a: number, b: number): number => {
 function subtrac(a: number, b: number): number {
   return a - b;
 }
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
